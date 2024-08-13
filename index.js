6a9abdef7439af454feb605334aab17f6b767394
David Huang <djhuang636@gmail.com>
2024-08-12 21:10:08 -0400

    create displayLibrary function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const myLibrary = [];
+const container = document.querySelector(".container");
+const booklist = document.createElement("ul");
+container.appendChild(booklist);
 
 function Book(title, author, pages, hasBeenRead) {
   this.title = title;
@@ -12,8 +15,25 @@ function Book(title, author, pages, hasBeenRead) {
   };
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary(...books) {
+  myLibrary.push(...books);
 }
 
-function displayLibrary() {}
+function displayLibrary() {
+  for (let item of myLibrary) {
+    const book = document.createElement("li");
+    console.log(item);
+    book.textContent = `Title: ${item.title}, Author: ${item.author}, pages: ${item.pages}
+    `;
+    booklist.appendChild(book);
+  }
+}
+
+const hobbit1 = new Book("The Hobbit1", "JRR Tolkien", 295, true);
+const hobbit2 = new Book("The Hobbit2", "JRR Tolkien", 296, true);
+const hobbit3 = new Book("The Hobbit3", "JRR Tolkien", 297, true);
+const hobbit4 = new Book("The Hobbit4", "JRR Tolkien", 298, true);
+
+console.log(hobbit1.title);
+addBookToLibrary(hobbit1, hobbit2, hobbit3, hobbit4);
+displayLibrary();
